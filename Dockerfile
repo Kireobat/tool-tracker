f9df2eab1da9cde7e1f75a9aa7e955f00667c4cb
Kireobat <erik.flatabo@proton.me>
2025-06-12 07:38:06 +0200

    la til datasouce ting i properties

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,10 @@ COPY ${JAR_FILE} application.jar
 RUN java -Djarmode=layertools -jar application.jar extract
 
 
-FROM eclipse-temurin:21-jre
+FROM eclipse-temurin:21-jre-alpine-3.21
 WORKDIR application
 COPY --from=builder application/dependencies/ ./
 COPY --from=builder application/spring-boot-loader/ ./
 COPY --from=builder application/application/ ./
+EXPOSE 8080
 ENTRYPOINT exec java org.springframework.boot.loader.launch.JarLauncher $JETTY_DEBUG -Xmx512M -Doracle.jdbc.J2EE13Compliant=true -Djava.security.egd=file:/dev/./urandom
---
+++ src/main/kotlin/eu/kireobat/tooltracker/api/controller/OpenController.kt
@@ -0,0 +1,19 @@
+package eu.kireobat.tooltracker.api.controller
+
+import eu.kireobat.tooltracker.api.dto.outbound.ToolTrackerResponseDto
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1")
+@ApiResponse(responseCode = "200", description = "OK")
+@ApiResponse(responseCode = "400", description = "Bad Request", content = [Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = Schema(implementation = ToolTrackerResponseDto::class))])
+@ApiResponse(responseCode = "500", description = "Internal Server Error", content = [Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = Schema(implementation = ToolTrackerResponseDto::class))])
+@Tag(name = "Open endpoints", description = "Every open endpoint that doesn't require auth")
+class OpenController {
+}
