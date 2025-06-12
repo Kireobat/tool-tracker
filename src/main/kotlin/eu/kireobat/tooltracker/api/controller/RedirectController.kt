0eef337f205e1dbdd0ce5558183c825d2ad33ae0
Kireobat <erik.flatabo@proton.me>
2025-06-12 09:40:56 +0200

    resten av redirect controlleren

--- src/main/kotlin/eu/kireobat/tooltracker/api/controller/RedirectController.kt
+++ src/main/kotlin/eu/kireobat/tooltracker/api/controller/RedirectController.kt
@@ -1,4 +1,17 @@
 package eu.kireobat.tooltracker.api.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
+
+@Tag(name="redirect")
+@RestController
 class RedirectController {
+    @Operation(hidden = true)
+    @GetMapping(path = ["/", "/swagger-ui"])
+    fun redirect(): RedirectView {
+        return RedirectView("/tool-tracker/swagger-ui/index.html")
+    }
 }
